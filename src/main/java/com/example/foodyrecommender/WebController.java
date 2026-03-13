6fd0f44f2b635e76b39f006074a37e685854f57d
Nguyen Duc Nghia <ducnghiabachong@gmail.com>
2026-03-13 11:31:03 +0700

    connect_database

--- src/main/java/com/example/foodyrecommender/WebController.java
+++ src/main/java/com/example/foodyrecommender/WebController.java
@@ -1,29 +1,38 @@
 package com.example.foodyrecommender;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
-public class WebController {
+@Configuration
+public class WebController implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/html/", "classpath:/css/", "classpath:/js/");
+    }
 
     @GetMapping("/")
     public String index() {
-        return "forward:/index.html";
+        return "index.html";
     }
 
     @GetMapping("/home")
     public String home() {
-        return "forward:/home.html";
+        return "home.html";
     }
 
     @GetMapping("/login")
     public String login() {
-        return "forward:/login.html";
+        return "login.html";
     }
 
     @GetMapping("/signup")
     public String signup() {
-        return "forward:/signup.html";
+        return "signup.html";
     }
 }
-
